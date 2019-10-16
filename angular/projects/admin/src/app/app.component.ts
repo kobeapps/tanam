8e5392eefae06f604ec11125cc89fb39f1c12832
Dennis Alund <dennis.alund@gmail.com>
2019-10-16 20:59:30 +0800

    Changing computers...

--- angular/projects/admin/src/app/app.component.ts
+++ angular/projects/admin/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tanam-root',
   template: '<router-outlet></router-outlet>',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tanam CMS';
   constructor() {
   }
